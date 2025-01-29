97d0c65e640957c0564c14ad6b564b31b44207b1
haythemch2 <haythem.chibani@gmail.com>
2025-01-29 23:13:34 +0100

    add parvum reports

---
+++ parvum/reports/js/abrs.js
@@ -0,0 +1,85 @@
+
+(function (d3) {
+
+    let dict = {
+        ab: ["Aminoglycoside"],
+        mb: ["Monobactam"],
+        pn: ["Penam"],
+        pe: ["Penem"],
+        cp: ["Carbapenem"],
+        cs: ["Cephalosporin"],
+        cm: ["Cephamycin"],
+        ce: ["Cycloserine"],
+        da: ["Diaminopyrimidine"],
+        em: ["Elfamycin"],
+        fl: ["Fluoroquinolone"],
+        fm: ["Fosfomycin"],
+        gl: ["Glycopeptide"],
+        ls: ["Lincosamide"],
+        lp: ["Lipopeptide"],
+        mc: ["Macrocyclic"],
+        ml: ["Macrolide"],
+        nf: ["Nitrofuran"],
+        ni: ["Nitroimidazole"],
+        nc: ["Nucleoside"],
+        ny: ["Nybomycin"],
+        oa: ["Organoarsenic"],
+        oz: ["Oxazolidinone"],
+        pt: ["Peptide"],
+        pc: ["Phenicol"],
+        pm: ["Pleuromutilin"],
+        pa: ["Polyamine"],
+        px: ["Polymyxin"],
+        ra: ["Rifamycin"],
+        sg: ["Streptogramin"],
+        sa: ["Sulfonamide"],
+        sf: ["Sulfone"],
+        tc: ["Tetracycline"],
+        ts: ["Triclosan"]
+    };
+    let dictLen = Object.keys(dict).length;
+
+    Object.keys(dict).forEach( (key, idx) => {
+        dict[key][1] = d3.schemeCategory20[idx%20];
+    } );
+
+    Array.from( document.getElementsByClassName( "abrs" ) ).forEach( (e) => {
+
+        let classList = e.classList;
+        let x = 8;
+        let y = 8;
+        let r = 8;
+        let xPlus = 18;
+
+        const svgContainer = d3.select(e).append( "svg" )
+            .attr( "width", (x + r) * dictLen + (dictLen - 1) * (xPlus - x - r) + 2)
+            .attr( "height", r*2 + 2)
+            .style( "background-color", "");
+
+        Object.keys(dict).forEach( (key, idx ) => {
+            let svg = svgContainer.append( "circle" )
+                .attr( 'class', 'abr-circle' )
+                .attr( "cx", (idx == 0) ? (x+1) : (x+1+xPlus*idx) )
+                .attr( "cy", y + 1 )
+                .attr( "r", r )
+                .attr( 'title', dict[key][0] )
+            if( gotABR( key, classList ) ) {
+                svg.attr( 'fill', dict[key][1] )
+            } else {
+                svg.attr( 'fill', 'white' )
+                   .attr( "stroke-width", 1 )
+                   .attr( "stroke", "#8ACAF5" );
+            }
+        } );
+
+    } );
+
+} )(d3);
+
+function gotABR( abr, abrList ) {
+    for( let i=0; i<abrList.length; i++) {
+        if( abr == abrList[i] ) return true;
+    }
+    return false;
+}
+
